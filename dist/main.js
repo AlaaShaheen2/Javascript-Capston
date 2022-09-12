1ce39a795d4734c18924aadb1ac295b132820dc1
inakiesk <inakieskruiz@gmail.com>
2022-09-12 13:50:47 -0500

    Add webpack config

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+/* comment */
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG8tZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiBjb21tZW50ICovIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
