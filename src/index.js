9d0d425a26a588061cf3e8194e91198f9c37241f
inakiesk <inakieskruiz@gmail.com>
2022-09-16 15:05:07 -0500

    Comments basic structure with basic API

--- src/index.js
+++ src/index.js
@@ -11,6 +11,99 @@ const fetchPoke = () => {
     pokemons.push(fetch(url).then((res) => res.json()));
   }
 
+  const displayComments = (pokemon) => {
+    const list = document.querySelector('.poke-elem');
+    list.addEventListener('click', (e) => {
+      if (e.target.innerHTML === "Comment") {
+
+        for (let i = 0; i <= pokemon.length; i += 1) {
+          if (e.target.parentElement.parentElement.firstChild.nextSibling.src === pokemon[i]?.image) {
+            
+            const commentWrapper = document.createElement('div');
+            commentWrapper.classList.add('comment-wrapper');
+            container.appendChild(commentWrapper);
+  
+            const commentSection = document.createElement('div');
+            commentSection.classList.add('comment-section');
+            commentWrapper.appendChild(commentSection);
+  
+            const closeDiv = document.createElement('div');
+            closeDiv.classList.add('close-div');
+            commentSection.appendChild(closeDiv);
+  
+            const closeIcon = new Image;
+            closeIcon.src = close;
+            closeIcon.classList.add('close');
+            closeDiv.appendChild(closeIcon);
+
+            const pokemonImg = new Image;
+            pokemonImg.src = pokemon[i].image;
+            pokemonImg.classList.add('img-card');
+            commentSection.appendChild(pokemonImg);
+
+            const pokemonName = document.createElement('h2');
+            pokemonName.classList.add('comment-pokename');
+            pokemonName.innerText = pokemon[i].name;
+            commentSection.appendChild(pokemonName);
+
+            const pokemonType = document.createElement("p");
+            pokemonType.classList.add('comment-poketype');
+            pokemonType.innerText = "Type: " + pokemon[i].type;
+            commentSection.appendChild(pokemonType);
+
+            const pokemonGameID = document.createElement("p");
+            pokemonGameID.classList.add("comment-pokeid");
+            pokemonGameID.innerText = "In-game Pokemon ID: " + pokemon[i].id
+            commentSection.appendChild(pokemonGameID);
+
+            const allCommentsWrapper = document.createElement("div");
+            allCommentsWrapper.classList.add("all-comments-wrapper");
+            commentSection.appendChild(allCommentsWrapper);
+
+            const commentsHeader = document.createElement("h2");
+            commentsHeader.classList.add("comments-header");
+            commentsHeader.innerText = "Comments";
+            allCommentsWrapper.appendChild(commentsHeader);
+
+            const allComments = document.createElement("ul");
+            allComments.classList.add("all-comments");
+            allCommentsWrapper.appendChild(allComments);
+
+            const addCommentWrapper = document.createElement("div");
+            addCommentWrapper.classList.add("add-comment-wrapper");
+            commentSection.appendChild(addCommentWrapper);
+
+            const addCommentHeader = document.createElement("h3");
+            addCommentHeader.classList.add("add-comment-header");
+            addCommentHeader.innerText = "Add a comment";
+            addCommentWrapper.appendChild(addCommentHeader);
+
+            const addCommentName = document.createElement("input");
+            addCommentName.classList.add("add-comment-name");
+            addCommentName.placeholder = "Your name";
+            addCommentWrapper.appendChild(addCommentName);
+
+            const addCommentText = document.createElement("textarea");
+            addCommentText.classList.add("add-comment-text");
+            addCommentText.placeholder = "Your comment";
+            addCommentWrapper.appendChild(addCommentText);
+
+            const addCommentBtn = document.createElement("button");
+            addCommentBtn.classList.add("add-comment-button");
+            addCommentBtn.innerText = "Comment";
+            addCommentWrapper.appendChild(addCommentBtn);
+
+            closeDiv.addEventListener('click', (z) => {
+              if (z.target.src === close) {
+                commentWrapper.remove();
+              }
+            });
+          }
+        }
+      };
+    });
+  }
+
   const showPoke = (pokemon) => {
     const pokeInnerHtml = pokemon
       .map(
@@ -21,8 +114,8 @@ const fetchPoke = () => {
             <h2 class="title-cr"> ${pok.name}</h2>
             <p class="type">Type: ${pok.type}</p>
             <div class = "click">
-            <button type="button" class="btn">Comment</button>
-            <img src="./images/icons8-heart-40(1).png" alt="" class="heart-img">
+              <button type="button" class="btn">Comment</button>
+              <img src="./images/icons8-heart-40(1).png" alt="" class="heart-img">
             </div>
         </li>
     `,
@@ -38,39 +131,8 @@ const fetchPoke = () => {
       id: res.id,
     }));
     showPoke(pokemon);
+    displayComments(pokemon);
   });
 };
 
 fetchPoke();
-
-function displayComments() {
-  const list = document.querySelector('.poke-elem');
-  list.addEventListener('click', (e) => {
-    if (e.target.innerHTML === "Comment") {
-      const commentWrapper = document.createElement('div');
-      commentWrapper.classList.add('comment-wrapper');
-      container.appendChild(commentWrapper);
-
-      const commentSection = document.createElement('div');
-      commentSection.classList.add('comment-section');
-      commentWrapper.appendChild(commentSection);
-
-      const closeDiv = document.createElement('div');
-      closeDiv.classList.add('close-div');
-      commentSection.appendChild(closeDiv);
-
-      const closeIcon = new Image;
-      closeIcon.src = close;
-      closeIcon.classList.add('close');
-      closeDiv.appendChild(closeIcon);
-
-      closeDiv.addEventListener('click', (z) => {
-        if (z.target.src === close) {
-          commentWrapper.remove();
-        }
-      });
-    };
-  });
-}
-
-displayComments();
