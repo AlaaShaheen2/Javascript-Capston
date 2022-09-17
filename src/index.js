5a0f8b9c163163b9ccf7f01f09ffe629911b4aa8
inakiesk <inakieskruiz@gmail.com>
2022-09-17 15:26:04 -0500

    Fix typo

--- src/index.js
+++ src/index.js
@@ -127,11 +127,11 @@ const fetchPoke = async () => {
                   addCommentText.value = '';
 
                   const reload = async () => {
-                    const waitComments = setTimeout(showComments, 500);
+                    const waitComments = setTimeout(showComments, 650);
+                    const waitRefresh = setTimeout(commentWrapper.remove(), 600);
                     requestComments();
+                    waitRefresh();
                     waitComments();
-                    commentWrapper.remove();
-                    showComments();
                   };
                   reload();
                 }
